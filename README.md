44e9c0d20e4c3ed7b65329dc5cf661165999550a
Alan Chow <hello@alanchow.ca>
2016-04-25 01:56:49 -0700

    add colour conversion functionality

---
+++ README.md
@@ -0,0 +1,10 @@
+# RGB To CMYK Colour Converter (React.js)
+
+## Summary
+A personal project to explore and learn more React.js. This project takes in RGB values via a slider input and converts the colour to the CMYK colour space.
+
+## To Do's
+I'd like to expand this project to take on the following capabilities:
+- Equivalent HEX colour
+- Colour Swatch generator (Complementary, Split-Complementary, Triad Relationship, etc.)
+- Other ideas as they come!
--- src/components/slider.jsx
+++ src/components/slider.jsx
@@ -1,13 +1,97 @@
 import React from 'react';
 
+var colourArray;
 var Slider = React.createClass({
+  getInitialState: function() {
+    return {
+      red: 0,
+      green: 0,
+      blue: 0,
+    }
+  },
+  _updateRed: function(e) {
+    this.setState({red:e.target.value})
+  },
+  _updateGreen: function(e) {
+    this.setState({green:e.target.value})
+  },
+  _updateBlue: function(e) {
+    this.setState({blue:e.target.value})
+  },
+  _convertCMYK: function(r,g,b) {
 
+    var r = r / 255;
+    var g = g / 255;
+    var b = b / 255;
+
+    var k = Math.min( 1 - r, 1 - g, 1 - b );
+    var c = ( 1 - r - k ) / ( 1 - k );
+    var m = ( 1 - g - k ) / ( 1 - k );
+    var y = ( 1 - b - k ) / ( 1 - k );
+
+    if(r==0 && g==0 && b==0) {
+      c = 0;
+      m = 0;
+      y = 0;
+      k = 100;
+      console.log(c, m, y, k);
+      colourArray = [c, m, y, k];
+    } else {
+      c = Math.round( c * 100 );
+      m = Math.round( m * 100 );
+      y = Math.round( y * 100 );
+      k = Math.round( k * 100 );
+      console.log(c, m, y, k);
+      colourArray = [c, m, y, k];
+    }
+  },
   render: function() {
+    var styles = {
+      background: 'rgb(' + this.state.red + ',' + this.state.green + ',' + this.state.blue + ')'
+    };
+    {this._convertCMYK(this.state.red,this.state.green,this.state.blue)}
     return (
       <div>
-        <label>Red</label><input type="range" />
+        <input id="red"
+          type="range"
+          min="0"
+          max="255"
+          steps="1"
+          value={this.state.red}
+          onChange={this._updateRed} />
+        <label>Red: {this.state.red}</label>
+        <input id="green"
+          type="range"
+          min="0"
+          max="255"
+          steps="1"
+          value={this.state.green}
+          onChange={this._updateGreen} />
+        <label>Green: {this.state.green}</label>
+        <input id="blue"
+          type="range"
+          min="0"
+          max="255"
+          steps="1"
+          value={this.state.blue}
+          onChange={this._updateBlue} />
+        <label>Blue: {this.state.blue}</label>
+        <div id="colour-block" style={styles}></div>
+        <ul className="cmyk-breakdown">
+          <li>C: {colourArray[0]}</li>
+          <li>M: {colourArray[1]}</li>
+          <li>Y: {colourArray[2]}</li>
+          <li>K: {colourArray[3]}</li>
+        </ul>
+        <div className="disclaimer">
+          <p>
+            CMYK Colour values and swatches are for reference only.
+            Please work with your printer to ensure colour accuracy!
+          </p>
+        </div>
       </div>
     );
+
   }
 });
 
